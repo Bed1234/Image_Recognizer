e0005088fd4c1ff0789098f1f92e54fa93085148
Bed1234 <76521252+Bed1234@users.noreply.github.com>
2021-07-24 09:05:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ function gotResult(error,results) {
      else {
        console.log(results);
        document.getElementById("result_object_name").innerHTML = results [0] .label;
+       document.getElementById("result_object_accuracy").innerHTML= results [0] .confidence.toFixed(3);
     }
 }
 
